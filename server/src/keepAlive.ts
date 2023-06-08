09414a7ed969dd5114db16909ecaf248483e7f9f
IvanXh0 <ivan.apostolovski@gmail.com>
2023-06-08 14:48:16 +0200

    Update keepAlive.ts

--- server/src/keepAlive.ts
+++ server/src/keepAlive.ts
@@ -1,10 +1,21 @@
 import * as cron from 'node-cron';
 import * as http from 'http';
 
-// Schedule the task to run every 13 minutes
-cron.schedule('*/13 * * * *', () => {
-  // Send a request to the '/ping' endpoint
+// Schedule the task to run every 10 minutes
+cron.schedule('*/10 * * * *', () => {
+  // Send a request to the '/api/snippets' endpoint
   http.get('https://codesnippet-prod.onrender.com/api/snippets', (res) => {
-    // Do nothing with the response
+    let data = '';
+
+    // Receive the response data
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    // Process the complete response
+    res.on('end', () => {
+      // Do something with the response data
+      console.log('Response:', data);
+    });
   });
 });
