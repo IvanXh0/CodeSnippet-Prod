e03725821e9693989a96856011210e88e8dd8761
IvanXh0 <ivan.apostolovski@gmail.com>
2023-06-10 21:11:09 +0200

    modal update

--- client/src/components/snippets/DeleteModal.jsx
+++ client/src/components/snippets/DeleteModal.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Box, Button, Modal, Typography } from '@mui/material';
 
-const DeleteModal = ({ open, handleClose, handleConfirm }) => {
+const DeleteModal = ({ open, handleClose, handleConfirm, anchorEl }) => {
   const handleDelete = () => {
     handleConfirm();
   };
@@ -10,45 +10,56 @@ const DeleteModal = ({ open, handleClose, handleConfirm }) => {
   return (
     <AnimatePresence>
       {open && (
-        <Modal open={open} onClose={handleClose} aria-labelledby="modal-title">
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-title"
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+        >
           <motion.div
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -50 }}
             transition={{ duration: 0.3 }}
             style={{
-              position: 'absolute',
-              top: '50%',
-              left: '50%',
-              transform: 'translate(-50%, -50%)',
               backgroundColor: 'white',
               padding: '2rem',
               borderRadius: '8px',
               boxShadow: '0 2px 8px rgba(0, 0, 0, 0.15)',
+              maxWidth: '400px',
             }}
           >
             <Box>
-              <Typography variant="h5" fontWeight="medium" id="modal-title">
+              <Typography
+                variant="h5"
+                fontWeight="bold"
+                id="modal-title"
+                gutterBottom
+              >
                 Are you sure you want to delete this snippet?
               </Typography>
-              <Typography variant="body1">
+              <Typography variant="body1" gutterBottom>
                 This action cannot be undone.
               </Typography>
             </Box>
 
-            <div style={{ marginTop: '1rem', textAlign: 'right' }}>
-              <Button variant="outlined" color="primary" onClick={handleClose}>
+            <Box display="flex" justifyContent="flex-end" marginTop="2rem">
+              <Button
+                variant="outlined"
+                color="primary"
+                onClick={handleClose}
+                style={{ marginRight: '1rem' }}
+              >
                 Cancel
               </Button>
-              <Button
-                variant="contained"
-                color="error"
-                onClick={handleDelete}
-                style={{ marginLeft: '1rem' }}
-              >
+              <Button variant="contained" color="error" onClick={handleDelete}>
                 Delete
               </Button>
-            </div>
+            </Box>
           </motion.div>
         </Modal>
       )}
