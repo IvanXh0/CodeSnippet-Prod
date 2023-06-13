9ec850acd384c3c2497a6464e425ccc15ac62b84
IvanXh0 <ivan.apostolovski@gmail.com>
2023-06-13 22:59:44 +0200

    Update cacheControl.middleware.ts

--- server/src/middlewares/cacheControl.middleware.ts
+++ server/src/middlewares/cacheControl.middleware.ts
@@ -4,7 +4,13 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CacheControlMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.setHeader('Cache-Control', 'public, max-age=31536000, must-revalidate');
+    const { url } = req;
+
+    // Check if the requested resource is an image
+    if (url.match(/\.(jpg|jpeg|png|gif|svg|webp)$/i)) {
+      res.setHeader('Cache-Control', 'public, max-age=31536000');
+    }
+
     next();
   }
 }
